6f4feb4ccd3714566f279a870567375f477d7364
Lykourgos Tsirikos <lykourgos.tsirikos@gmail.com>
2020-10-10 09:58:35 +0100

    Create a base layout, pages and components

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 $app->get('/', function ($request, $response, $args) use ($container) {
     // $response->getBody()->write();
-    return $container->get('view')->render($response, 'home.twig', ['foo' => 'test']);
+    return $container->get('view')->render($response, 'pages/home.twig', ['foo' => 'test']);
 })->setName('home');
 
 $app->get('/about', function ($request, $response, $args) use ($container) {
-    return $container->get('view')->render($response, 'about.twig');
+    return $container->get('view')->render($response, 'pages/about.twig');
 })->setName('about');
